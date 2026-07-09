ec1e8cfa2769f70765c2c040f290e5e9637773d4
n-sweep <34486798+n-sweep@users.noreply.github.com>
2026-07-09 16:16:22 -0400

    cleanup

--- _lua/foundry/utils.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
---- Check if the current buffer is a Quarto document.
---- @return boolean is_quarto True if filetype is 'quarto'
-function M.is_quarto()
-    return vim.bo.filetype == 'quarto'
-end
-
-return M
